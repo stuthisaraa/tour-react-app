952a8f84f9ae9c3d900044a35813ed195a2e2a82
stuthisaraa <stuthisara@gmail.com>
2024-03-14 10:45:50 +0530

    First commit

---
+++ src/component/Cardcontainer.js
@@ -0,0 +1,24 @@
+import react from 'react'
+import Card from './Card';
+
+const Cardcontainer = ({data})=>{
+    return(
+        <div className="filter-container">
+
+            <div className='cards'>
+            {
+                data.map((item)=>(
+                    <Card key={item.id} {...item}></Card>
+                ))
+            }
+
+            </div>
+
+            
+        </div>
+    )
+
+
+}
+
+export default Cardcontainer;
---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Header = ()=>{
+    return(
+        <div className="container">
+            <div className="title">
+            Plan A Tour
+            </div>
+        </div>
+    )
+}
+    export default Header;
