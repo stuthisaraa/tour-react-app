31d8ff9110a305d598c095b0659a2473c4edc08e
stuthisaraa <153415969+stuthisaraa@users.noreply.github.com>
2024-03-14 12:28:26 +0530

    new

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ const Header = ()=>{
     return(
         <div className="container">
             <div className="title">
-            Plan A Tour
+            Tours
             </div>
         </div>
     )
